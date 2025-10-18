2a371d4a0903badea27afa29111b857d27047e52
osm1019 <osm1019@gmail.com>
2025-10-17 22:35:47 -0700

    Fix Up! add missing preference

--- DeviceSettings/src/org/lineageos/device/DeviceSettings/CustomSeekBarPreference.java
+++ DeviceSettings/src/org/lineageos/device/DeviceSettings/CustomSeekBarPreference.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.lineageos.device.settings.preferences;
+package org.lineageos.device.DeviceSettings.preferences;
 
 import android.content.Context;
 import android.content.res.TypedArray;
