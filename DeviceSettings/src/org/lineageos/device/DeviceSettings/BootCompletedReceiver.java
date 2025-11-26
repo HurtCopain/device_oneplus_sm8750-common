AlphaDroid-devices/device_oneplus_aston

DeviceSettings: refactor and feature additions
* Refactor bypass charging with improved state management
  - Split BypassChargingService into BypassChargingController (state) and BypassChargingManager (service)
  - Add per-app bypass charging support with app list management
  - Implement proper power connect/disconnect handling
  - Add detailed notification states with battery level tracking

* Add HBM (High Brightness Mode) support
  - Implement HbmController with PWM priority handling
  - Add HBM Quick Settings tile
  - Auto-adjust refresh rate to 90Hz when HBM is active
  - Add warning dialog for panel degradation

* Add per-app refresh rate control
  - Implement RefreshRateController for global and per-app rates
  - Add RefreshRateMonitorService with foreground app detection
  - Support 60/90/120Hz and auto modes
  - Add custom AppRefreshRatePreference with delete functionality

* Refactor GameBar overlay
  - Rewrite GameBar with fixed-width layout and better UI
  - Implement proper app monitoring with shared ForegroundAppDetector
  - Add per-app auto-enable support
  - Improve side-by-side layout with dynamic item filtering
  - Add proper bounds checking for draggable mode
  - Consolidate app selector into main settings

* Add shared utilities
  - ForegroundAppDetector: Event-driven app monitoring with TaskStackListener
  - AppListManager: Unified app list persistence and management
  - AppPreferencesHelper: Common preference UI operations
  - PackageListAdapter: Reusable app selection dialog

* UI improvements
  - Fix DeviceSettings banner/mask rendering with proper fade animation
  - Separate PWM and HBM controls with mutual exclusivity
  - Add proper delete confirmations for app lists
  - Improve preference summaries and validation

* Code cleanup
  - Remove unused GameBar app selector activities and layouts
  - Improve error handling and state synchronization
  - Add proper service lifecycle management

Change-Id: Id9b034eb66a59cede4406ba56353cdc81a6b8819

device-settings/src/org/lineageos/device/settings/BootCompletedReceiver.java
@@ -14,41 +14,86 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.lineageos.device.settings;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.text.TextUtils;
+import android.util.Log;
 
 import androidx.preference.PreferenceManager;
 
+import org.lineageos.device.settings.bypasschrg.BypassChargingController;
+import org.lineageos.device.settings.bypasschrg.BypassChargingManager;
 import org.lineageos.device.settings.gamebar.GameBar;
+import org.lineageos.device.settings.gamebar.GameBarController;
 import org.lineageos.device.settings.gamebar.GameBarMonitorService;
+import org.lineageos.device.settings.refreshrate.RefreshRateController;
+import org.lineageos.device.settings.refreshrate.RefreshRateMonitorService;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
+    private static final String TAG = "BootCompletedReceiver";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
         String action = intent.getAction();
         if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
-            restoreGameBarOverlayState(context);
+            if (Constants.DEBUG) Log.i(TAG, "Boot completed - initializing services");
+            initializeBypassCharging(context);
+            initializeGameBar(context);
+            initializeRefreshRate(context);
         }
     }
 
-    private void restoreGameBarOverlayState(Context context) {
-        var prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        boolean mainEnabled = prefs.getBoolean("game_bar_enable", false);
-        boolean autoEnabled = prefs.getBoolean("game_bar_auto_enable", false);
-        if (mainEnabled) {
-            // Restore game bar preferences
-            GameBar.getInstance(context).applyPreferences();
-            GameBar.getInstance(context).show();
+    private void initializeBypassCharging(Context context) {
+        if (Constants.DEBUG) Log.i(TAG, "Initializing BypassCharging");
+        try {
+            BypassChargingController controller = BypassChargingController.getInstance(context);
+            BypassChargingManager.notifyStateChanged(context, controller.getState());
+            if (Constants.DEBUG) Log.i(TAG, "BypassCharging initialized");
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to initialize BypassCharging", e);
         }
-        if (autoEnabled) {
-            // Start GameBarMonitorService
-            Intent monitorIntent = new Intent(context, GameBarMonitorService.class);
-            context.startService(monitorIntent);
+    }
+
+    private void initializeGameBar(Context context) {
+        if (Constants.DEBUG) Log.i(TAG, "Initializing GameBar");
+        try {
+            var prefs = PreferenceManager.getDefaultSharedPreferences(context);
+            boolean masterEnabled = prefs.getBoolean("game_bar_enable", false);
+            boolean hasAutoApps = hasGameBarAutoApps(context);
+            if (Constants.DEBUG) Log.i(TAG, "GameBar state: masterEnabled=" + masterEnabled + ", hasAutoApps=" + hasAutoApps);
+            if (masterEnabled) {
+                // Restore GameBar overlay state
+                GameBar.getInstance(context).applyPreferences();
+                GameBar.getInstance(context).show();
+                if (Constants.DEBUG) Log.i(TAG, "GameBar overlay restored");
+            }
+            if (masterEnabled || hasAutoApps) {
+                // Start monitoring service
+                GameBarMonitorService.notifyStateChanged(context);
+                if (Constants.DEBUG) Log.i(TAG, "GameBar monitoring service initialized");
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to initialize GameBar", e);
         }
+    }
+
+    private void initializeRefreshRate(Context context) {
+        if (Constants.DEBUG) Log.i(TAG, "Initializing RefreshRate");
+        try {
+            RefreshRateController controller = RefreshRateController.getInstance(context);
+            RefreshRateMonitorService.notifyStateChanged(context);
+            if (Constants.DEBUG) Log.i(TAG, "RefreshRate initialized");
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to initialize RefreshRate", e);
+        }
+    }
+
+    private boolean hasGameBarAutoApps(Context context) {
+        String raw = PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(Constants.KEY_GAMEBAR_AUTO_APPS, "");
+        return !TextUtils.isEmpty(raw);
     }
 }

HurtCopain/device_oneplus_sm8750-common

DeviceSettings/src/org/lineageos/device/DeviceSettings/BootCompletedReceiver.java
@@ -20,35 +20,81 @@ package org.lineageos.device.DeviceSettings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.text.TextUtils;
+import android.util.Log;
 
 import androidx.preference.PreferenceManager;
 
+import org.lineageos.device.DeviceSettings.bypasschrg.BypassChargingController;
+import org.lineageos.device.DeviceSettings.bypasschrg.BypassChargingManager;
 import org.lineageos.device.DeviceSettings.gamebar.GameBar;
+import org.lineageos.device.DeviceSettings.gamebar.GameBarController;
 import org.lineageos.device.DeviceSettings.gamebar.GameBarMonitorService;
+import org.lineageos.device.DeviceSettings.refreshrate.RefreshRateController;
+import org.lineageos.device.DeviceSettings.refreshrate.RefreshRateMonitorService;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
+    private static final String TAG = "BootCompletedReceiver";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
         String action = intent.getAction();
         if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
-            restoreGameBarOverlayState(context);
+            if (Constants.DEBUG) Log.i(TAG, "Boot completed - initializing services");
+            initializeBypassCharging(context);
+            initializeGameBar(context);
+            initializeRefreshRate(context);
         }
     }
 
-    private void restoreGameBarOverlayState(Context context) {
-        var prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        boolean mainEnabled = prefs.getBoolean("game_bar_enable", false);
-        boolean autoEnabled = prefs.getBoolean("game_bar_auto_enable", false);
-        if (mainEnabled) {
-            // Restore game bar preferences
-            GameBar.getInstance(context).applyPreferences();
-            GameBar.getInstance(context).show();
+    private void initializeBypassCharging(Context context) {
+        if (Constants.DEBUG) Log.i(TAG, "Initializing BypassCharging");
+        try {
+            BypassChargingController controller = BypassChargingController.getInstance(context);
+            BypassChargingManager.notifyStateChanged(context, controller.getState());
+            if (Constants.DEBUG) Log.i(TAG, "BypassCharging initialized");
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to initialize BypassCharging", e);
         }
-        if (autoEnabled) {
-            // Start GameBarMonitorService
-            Intent monitorIntent = new Intent(context, GameBarMonitorService.class);
-            context.startService(monitorIntent);
+    }
+
+    private void initializeGameBar(Context context) {
+        if (Constants.DEBUG) Log.i(TAG, "Initializing GameBar");
+        try {
+            var prefs = PreferenceManager.getDefaultSharedPreferences(context);
+            boolean masterEnabled = prefs.getBoolean("game_bar_enable", false);
+            boolean hasAutoApps = hasGameBarAutoApps(context);
+            if (Constants.DEBUG) Log.i(TAG, "GameBar state: masterEnabled=" + masterEnabled + ", hasAutoApps=" + hasAutoApps);
+            if (masterEnabled) {
+                // Restore GameBar overlay state
+                GameBar.getInstance(context).applyPreferences();
+                GameBar.getInstance(context).show();
+                if (Constants.DEBUG) Log.i(TAG, "GameBar overlay restored");
+            }
+            if (masterEnabled || hasAutoApps) {
+                // Start monitoring service
+                GameBarMonitorService.notifyStateChanged(context);
+                if (Constants.DEBUG) Log.i(TAG, "GameBar monitoring service initialized");
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to initialize GameBar", e);
         }
+    }
+
+    private void initializeRefreshRate(Context context) {
+        if (Constants.DEBUG) Log.i(TAG, "Initializing RefreshRate");
+        try {
+            RefreshRateController controller = RefreshRateController.getInstance(context);
+            RefreshRateMonitorService.notifyStateChanged(context);
+            if (Constants.DEBUG) Log.i(TAG, "RefreshRate initialized");
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to initialize RefreshRate", e);
+        }
+    }
+
+    private boolean hasGameBarAutoApps(Context context) {
+        String raw = PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(Constants.KEY_GAMEBAR_AUTO_APPS, "");
+        return !TextUtils.isEmpty(raw);
     }
 }