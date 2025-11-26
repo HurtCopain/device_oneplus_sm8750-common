AlphaDroid-devices/device_oneplus_aston

DeviceSettings: rework bypass charging
* Drop Lineage Health dependency
* Turn bypass charging into a tri-state (0-BYPASS_OFF, 1-BYPASS_WAITING, 2-BYPASS_OFF
* Add target limit

Change-Id: I06b6e9e8da7ffc20c769d47627d07cc189e884b4

device-settings/src/org/lineageos/device/settings/bypasschrg/BypassChargingFragment.java
@@ -21,11 +21,11 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.TwoStatePreference;
 
+import org.lineageos.device.settings.Constants;
 import org.lineageos.device.settings.R;
+import org.lineageos.device.settings.preferences.CustomSeekBarPreference;
 
 public class BypassChargingFragment extends PreferenceFragmentCompat {
-
-    private static final String KEY_BYPASS_CHARGING = "bypass_charging";
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -35,16 +35,33 @@ public class BypassChargingFragment extends PreferenceFragmentCompat {
                 BypassChargingController.getInstance(getContext());
         boolean bypassSupported = bypassController.isBypassChargingSupported();
 
-        TwoStatePreference bypassPreference = findPreference(KEY_BYPASS_CHARGING);
-        bypassPreference.setEnabled(bypassSupported);
-        if (bypassSupported) {
-            bypassPreference.setChecked(bypassController.isBypassChargingEnabled());
-            bypassPreference.setOnPreferenceChangeListener((pref, newValue) -> {
-                bypassController.setBypassCharging((boolean) newValue);
-                return true;
-            });
-        } else {
-            bypassPreference.setSummary(R.string.bypass_charging_unavailable);
+        TwoStatePreference bypassPreference = findPreference(Constants.KEY_BYPASS_CHARGING);
+        if (bypassPreference != null) {
+            bypassPreference.setEnabled(bypassSupported);
+            if (bypassSupported) {
+                bypassPreference.setChecked(bypassController.getBypassChargingStatus() != Constants.BYPASS_OFF);
+                bypassPreference.setOnPreferenceChangeListener((pref, newValue) -> {
+                    boolean enable = (boolean) newValue;
+                    if (enable) {
+                        bypassController.enableBypassCharging();
+                    }
+                    else {
+                        bypassController.disableBypassCharging();
+                    }
+                    return true;
+                });
+            } else {
+                bypassPreference.setSummary(R.string.bypass_charging_unavailable);
+            }
+        }
+
+        CustomSeekBarPreference targetPreference = findPreference(Constants.KEY_BYPASS_CHARGING_TARGET);
+        if (targetPreference != null) {
+            targetPreference.setValue(bypassController.getBypassChargingTarget());
+            targetPreference.setOnPreferenceChangeListener((pref, newValue) -> {
+                    bypassController.setBypassChargingTarget((int) newValue);
+                    return true;
+                });
         }
     }
 }

device-settings/src/org/lineageos/device/settings/bypasschrg/BypassChargingService.java
@@ -1,0 +1,133 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.device.settings.bypasschrg;
+
+import static android.os.BatteryManager.EXTRA_LEVEL;
+import static android.os.BatteryManager.EXTRA_SCALE;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.os.IBinder;
+import android.os.UserHandle;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import org.lineageos.device.settings.Constants;
+import org.lineageos.device.settings.R;
+
+public class BypassChargingService extends Service {
+    public static final String CHANNEL_ID = "bypass_charging_channel";
+    public static final int NOTIF_ID = 1337;
+    private static final String TAG = "BypassChargingService";
+
+    private BypassChargingController mController;
+    private BroadcastReceiver mBatteryReceiver;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mController = BypassChargingController.getInstance(getApplicationContext());
+        createNotificationChannel();
+        startForeground(NOTIF_ID, buildNotification());
+        registerBatteryReceiver();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterBatteryReceiver();
+        stopForeground(true);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null; // Not a bound service
+    }
+
+    private void registerBatteryReceiver() {
+        if (mBatteryReceiver != null) return;
+        mBatteryReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int extraLevel = intent.getIntExtra(EXTRA_LEVEL, -1);
+                int extraScale = intent.getIntExtra(EXTRA_SCALE, -1);
+                int level = (extraLevel >= 0 && extraScale > 0) ?
+                        (int) ((extraLevel / (float) extraScale) * 100) : -1;
+
+                Log.d(TAG, "Battery level in service: " + level);
+                mController.onBatteryLevelChanged(level);
+            }
+        };
+        IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(mBatteryReceiver, filter);
+    }
+
+    private void unregisterBatteryReceiver() {
+        if (mBatteryReceiver != null) {
+            try {
+                unregisterReceiver(mBatteryReceiver);
+            } catch (Exception e) {}
+            mBatteryReceiver = null;
+        }
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    getString(R.string.bypass_charging_title),
+                    NotificationManager.IMPORTANCE_LOW
+            );
+            channel.setDescription(getString(R.string.bypass_charging_waiting));
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            if (manager != null) manager.createNotificationChannel(channel);
+        }
+    }
+
+    private Notification buildNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle(getString(R.string.bypass_charging_title))
+                .setContentText(getString(R.string.bypass_charging_waiting))
+                .setSmallIcon(R.drawable.ic_bypass_charging)
+                .setOngoing(true)
+                .setPriority(NotificationCompat.PRIORITY_LOW);
+        return builder.build();
+    }
+
+    // Static method to start/stop service from anywhere
+    public static void start(Context context) {
+        Intent intent = new Intent(context, BypassChargingService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundServiceAsUser(intent, UserHandle.CURRENT);
+        } else {
+            context.startService(intent);
+        }
+    }
+
+    public static void stop(Context context) {
+        Intent intent = new Intent(context, BypassChargingService.class);
+        context.stopService(intent);
+    }
+}

device-settings/src/org/lineageos/device/settings/bypasschrg/BypassChargingTile.java
@@ -16,9 +16,11 @@
 
 package org.lineageos.device.settings.bypasschrg;
 
+import android.graphics.drawable.Icon;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 
+import org.lineageos.device.settings.Constants;
 import org.lineageos.device.settings.R;
 
 public class BypassChargingTile extends TileService {
@@ -35,26 +37,42 @@ public class BypassChargingTile extends TileService {
 
     @Override
     public void onStartListening() {
-        mEnabled = mBypassController.isBypassChargingEnabled();
-        updateTileState();
+        int status = mBypassController.getBypassChargingStatus();
+        mEnabled = status != Constants.BYPASS_OFF;
+        updateTileState(status);
     }
 
     @Override
     public void onClick() {
-        if (mEnabled == mBypassController.isBypassChargingEnabled()) {
-            mEnabled = !mEnabled;
-            updateTileState();
-            mBypassController.setBypassCharging(mEnabled);
+        boolean enabled = mBypassController.getBypassChargingStatus() != Constants.BYPASS_OFF;
+        if (mEnabled == enabled) {
+            boolean success;
+            if (mEnabled) {
+                success = mBypassController.disableBypassCharging() ? true : false;
+            }
+            else {
+                success = mBypassController.enableBypassCharging() ? true : false;
+            }
+            if (success) {
+                mEnabled = !mEnabled;
+                updateTileState(mBypassController.getBypassChargingStatus());
+            }
         }
     }
 
-    private void updateTileState() {
+    private void updateTileState(int status) {
         Tile tile = getQsTile();
         if (tile == null) return;
 
-        tile.setState(mEnabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        tile.setState(status==Constants.BYPASS_OFF ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
         tile.setLabel(getString(R.string.bypass_charging_title));
         tile.setContentDescription(getString(R.string.bypass_charging_summary));
+        if (status==Constants.BYPASS_WAITING) {
+            tile.setIcon(Icon.createWithResource(this, R.drawable.ic_bypass_waiting));
+        }
+        else {
+            tile.setIcon(Icon.createWithResource(this, R.drawable.ic_bypass_charging));
+        }
         tile.updateTile();
     }
 }

HurtCopain/device_oneplus_sm8750-common

DeviceSettings/src/org/lineageos/device/DeviceSettings/bypasschrg/BypassChargingFragment.java
@@ -21,11 +21,11 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.TwoStatePreference;
 
+import org.lineageos.device.DeviceSettings.Constants;
 import org.lineageos.device.DeviceSettings.R;
+import org.lineageos.device.DeviceSettings.preferences.CustomSeekBarPreference;
 
 public class BypassChargingFragment extends PreferenceFragmentCompat {
-
-    private static final String KEY_BYPASS_CHARGING = "bypass_charging";
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -35,16 +35,33 @@ public class BypassChargingFragment extends PreferenceFragmentCompat {
                 BypassChargingController.getInstance(getContext());
         boolean bypassSupported = bypassController.isBypassChargingSupported();
 
-        TwoStatePreference bypassPreference = findPreference(KEY_BYPASS_CHARGING);
-        bypassPreference.setEnabled(bypassSupported);
-        if (bypassSupported) {
-            bypassPreference.setChecked(bypassController.isBypassChargingEnabled());
-            bypassPreference.setOnPreferenceChangeListener((pref, newValue) -> {
-                bypassController.setBypassCharging((boolean) newValue);
-                return true;
-            });
-        } else {
-            bypassPreference.setSummary(R.string.bypass_charging_unavailable);
+        TwoStatePreference bypassPreference = findPreference(Constants.KEY_BYPASS_CHARGING);
+        if (bypassPreference != null) {
+            bypassPreference.setEnabled(bypassSupported);
+            if (bypassSupported) {
+                bypassPreference.setChecked(bypassController.getBypassChargingStatus() != Constants.BYPASS_OFF);
+                bypassPreference.setOnPreferenceChangeListener((pref, newValue) -> {
+                    boolean enable = (boolean) newValue;
+                    if (enable) {
+                        bypassController.enableBypassCharging();
+                    }
+                    else {
+                        bypassController.disableBypassCharging();
+                    }
+                    return true;
+                });
+            } else {
+                bypassPreference.setSummary(R.string.bypass_charging_unavailable);
+            }
+        }
+
+        CustomSeekBarPreference targetPreference = findPreference(Constants.KEY_BYPASS_CHARGING_TARGET);
+        if (targetPreference != null) {
+            targetPreference.setValue(bypassController.getBypassChargingTarget());
+            targetPreference.setOnPreferenceChangeListener((pref, newValue) -> {
+                    bypassController.setBypassChargingTarget((int) newValue);
+                    return true;
+                });
         }
     }
 }

DeviceSettings/src/org/lineageos/device/DeviceSettings/bypasschrg/BypassChargingService.java
@@ -1,0 +1,133 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.device.DeviceSettings.bypasschrg;
+
+import static android.os.BatteryManager.EXTRA_LEVEL;
+import static android.os.BatteryManager.EXTRA_SCALE;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.os.IBinder;
+import android.os.UserHandle;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import org.lineageos.device.DeviceSettings.Constants;
+import org.lineageos.device.DeviceSettings.R;
+
+public class BypassChargingService extends Service {
+    public static final String CHANNEL_ID = "bypass_charging_channel";
+    public static final int NOTIF_ID = 1337;
+    private static final String TAG = "BypassChargingService";
+
+    private BypassChargingController mController;
+    private BroadcastReceiver mBatteryReceiver;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mController = BypassChargingController.getInstance(getApplicationContext());
+        createNotificationChannel();
+        startForeground(NOTIF_ID, buildNotification());
+        registerBatteryReceiver();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterBatteryReceiver();
+        stopForeground(true);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null; // Not a bound service
+    }
+
+    private void registerBatteryReceiver() {
+        if (mBatteryReceiver != null) return;
+        mBatteryReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                int extraLevel = intent.getIntExtra(EXTRA_LEVEL, -1);
+                int extraScale = intent.getIntExtra(EXTRA_SCALE, -1);
+                int level = (extraLevel >= 0 && extraScale > 0) ?
+                        (int) ((extraLevel / (float) extraScale) * 100) : -1;
+
+                Log.d(TAG, "Battery level in service: " + level);
+                mController.onBatteryLevelChanged(level);
+            }
+        };
+        IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(mBatteryReceiver, filter);
+    }
+
+    private void unregisterBatteryReceiver() {
+        if (mBatteryReceiver != null) {
+            try {
+                unregisterReceiver(mBatteryReceiver);
+            } catch (Exception e) {}
+            mBatteryReceiver = null;
+        }
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    getString(R.string.bypass_charging_title),
+                    NotificationManager.IMPORTANCE_LOW
+            );
+            channel.setDescription(getString(R.string.bypass_charging_waiting));
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            if (manager != null) manager.createNotificationChannel(channel);
+        }
+    }
+
+    private Notification buildNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle(getString(R.string.bypass_charging_title))
+                .setContentText(getString(R.string.bypass_charging_waiting))
+                .setSmallIcon(R.drawable.ic_bypass_charging)
+                .setOngoing(true)
+                .setPriority(NotificationCompat.PRIORITY_LOW);
+        return builder.build();
+    }
+
+    // Static method to start/stop service from anywhere
+    public static void start(Context context) {
+        Intent intent = new Intent(context, BypassChargingService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundServiceAsUser(intent, UserHandle.CURRENT);
+        } else {
+            context.startService(intent);
+        }
+    }
+
+    public static void stop(Context context) {
+        Intent intent = new Intent(context, BypassChargingService.class);
+        context.stopService(intent);
+    }
+}

DeviceSettings/src/org/lineageos/device/DeviceSettings/bypasschrg/BypassChargingTile.java
@@ -16,9 +16,12 @@
 
 package org.lineageos.device.DeviceSettings.bypasschrg;
 
+import android.graphics.drawable.Icon;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 
+
+import org.lineageos.device.DeviceSettings.Constants;
 import org.lineageos.device.DeviceSettings.R;
 
 public class BypassChargingTile extends TileService {
@@ -35,26 +38,42 @@ public class BypassChargingTile extends TileService {
 
     @Override
     public void onStartListening() {
-        mEnabled = mBypassController.isBypassChargingEnabled();
-        updateTileState();
+        int status = mBypassController.getBypassChargingStatus();
+        mEnabled = status != Constants.BYPASS_OFF;
+        updateTileState(status);
     }
 
     @Override
     public void onClick() {
-        if (mEnabled == mBypassController.isBypassChargingEnabled()) {
-            mEnabled = !mEnabled;
-            updateTileState();
-            mBypassController.setBypassCharging(mEnabled);
+        boolean enabled = mBypassController.getBypassChargingStatus() != Constants.BYPASS_OFF;
+        if (mEnabled == enabled) {
+            boolean success;
+            if (mEnabled) {
+                success = mBypassController.disableBypassCharging() ? true : false;
+            }
+            else {
+                success = mBypassController.enableBypassCharging() ? true : false;
+            }
+            if (success) {
+                mEnabled = !mEnabled;
+                updateTileState(mBypassController.getBypassChargingStatus());
+            }
         }
     }
 
-    private void updateTileState() {
+    private void updateTileState(int status) {
         Tile tile = getQsTile();
         if (tile == null) return;
 
-        tile.setState(mEnabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        tile.setState(status==Constants.BYPASS_OFF ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
         tile.setLabel(getString(R.string.bypass_charging_title));
         tile.setContentDescription(getString(R.string.bypass_charging_summary));
+        if (status==Constants.BYPASS_WAITING) {
+            tile.setIcon(Icon.createWithResource(this, R.drawable.ic_bypass_waiting));
+        }
+        else {
+            tile.setIcon(Icon.createWithResource(this, R.drawable.ic_bypass_charging));
+        }
         tile.updateTile();
     }
 }